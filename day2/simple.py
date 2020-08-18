fef64dbab61697ba95ab1bf1e5beb4821e81bf79
seanaleid <seanaleid@gmail.com>
2020-08-18 12:23:18 -0400

    Add day 2 folder and files

--- day2/simple.py
+++ day2/simple.py
@@ -1,3 +1,4 @@
+  
 # Let's build a data driven machine!
 
 # What do we need to have our machine working?
@@ -5,24 +6,31 @@
 - Some sort of memory
 - Some way of stopping operation
 - Some way of keeping the CPU running
-- Some sort of storage for local variables separate from main RAM (memory) eg; Registers
+- Some sort of storage for local variables seperate from main RAM (memory) eg; Registers
 - Some sort of operations that can be performed such as (printing something, saving data to a variable[register] )
 - Some FETCH, DECODE, EXECUTE CYCLE
-
 """
 
 # Operations that we can perform
 PRINT_VLAD = 1
 HALT = 2
 PRINT_NUM = 3
-PRINT_REG = 4 # PRN
-SAVE = 5 # LDI
+PRINT_REG = 4
+SAVE = 5
 ADD = 6
-SUB = 7
+SUB = 23
 LDI = 0b10000010
 PRN = 0b01000111
-# some sort of memory
+
+# some sort of memory (lets refactor this to load in opcodes from a file)
+
+def load_memory(filename):
+    # TODO do some logic here
+    pass
+
 mem = [0] * 256
+
+
 memory = [
     PRINT_VLAD,
     SAVE,
@@ -38,25 +46,26 @@ memory = [
     3,
     PRINT_REG,
     2,
-    SUB,
-    2,
-    3,
-    PRINT_REG,
-    2,
     PRINT_NUM,
     120,
     PRINT_VLAD,
-    HALT,
+    HALT
 ]
 # keep track of running?
 running = True
-# Some sort of counter --> program counter
+
+# some sort of counter
 pc = 0
 # Some local var holders [registers]
 registers = [0] * 10
 
 # size of opcode
 op_size = 1
+
+# TODO: grab any args
+
+# TODO: load opcodes in to memory
+
 
 # REPL to run once per cycle of CPU
 # inside this we will have our FETCH, DECODE, EXECUTE CYCLE
@@ -67,36 +76,39 @@ while running:
     # DECODE
     if cmd == PRINT_VLAD:
         # EXECUTE
-        print('Vlad')
+        print("Vlad")
         op_size = 1
     elif cmd == HALT:
         running = False
     elif cmd == PRINT_NUM:
-        num = memory[pc+1]
+        num = memory[pc + 1]
         print(num)
         op_size = 2
     elif cmd == PRINT_REG:
-        index_of_reg = memory[pc+1]
+        index_of_reg = memory[pc + 1]
         num_at_reg = registers[index_of_reg]
         print(num_at_reg)
         op_size = 2
     elif cmd == SAVE:
-        num_to_save = memory[pc+1] # 300
-        reg_index = memory[pc+2] 
+        num_to_save = memory[pc + 1] # 300
+        reg_index = memory[pc + 2]
+
         registers[reg_index] = num_to_save
-        # print(registers[reg_index])
+
         op_size = 3
     elif cmd == ADD:
-        reg_index_a = memory[pc+1]
-        reg_index_b = memory[pc+2]
+        reg_index_a = memory[pc + 1]
+        reg_index_b = memory[pc + 2]
         registers[reg_index_a] += registers[reg_index_b]
 
         op_size = 3
+
     elif cmd == SUB:
-        reg_index_a = memory[pc+1]
-        reg_index_b = memory[pc+2]
+        reg_index_a = memory[pc + 1]
+        reg_index_b = memory[pc + 2]
         registers[reg_index_a] -= registers[reg_index_b]
 
         op_size = 3
+
 
     pc += op_size
